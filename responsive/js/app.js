8e674f71094740de25cdb6f0ce167fc1edf3d791
Hedy <hedywoo@gmail.com>
2016-03-18 13:55:34 -0500

    binding select, and getting rid of button

--- responsive/js/app.js
+++ responsive/js/app.js
@@ -10,19 +10,26 @@ $("#menu a").each(function()) {
   vor $anchor = $(this);
   //create an option
   var $option = $("<option></optiona>);
+  //deal with selected options depending on current page 
+  if($anchor.parent().hasClass(".selected")) {
+    $option.prop( 'selected', true);;
+  }
                   //option's value is the href
                   $option.val($anchor.attr("href"));
                   //option's text is the text of link 
                   $option.text($anchor.text());
                   //append option to select
                   $select.append($option);
-
 });
-//Create button 
-var $button = $("<button> Go</button> ");
-$("#menu").append($button);
-//Bind click to button
-$button.click(function() {
+//bind change listener to the selected
+$select.change(function() {
+
   //Go to 'select's location
   window.location = $select.val();
 });
+
+
+
+//modify CSS to hide links on mall width and show button and select
+//also hides select and button on larger width and show's links
+//deal with selected options depending on current page
